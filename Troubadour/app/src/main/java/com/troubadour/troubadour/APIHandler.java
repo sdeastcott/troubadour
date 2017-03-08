00a7f41c7aa66aa629499df430c713b146d82de5
jblairkiel <jblairkiel@gmail.com>
2017-03-08 01:47:59 -0600

    Receiving 500 response error from Server, Marcus pls

--- Troubadour/app/src/main/java/com/troubadour/troubadour/APIHandler.java
+++ Troubadour/app/src/main/java/com/troubadour/troubadour/APIHandler.java
@@ -1,5 +1,25 @@
 package com.troubadour.troubadour;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 import kaaes.spotify.webapi.android.SpotifyApi;
 
 /**
@@ -9,8 +29,103 @@ import kaaes.spotify.webapi.android.SpotifyApi;
 public class APIHandler {
 
     SpotifyApi api;
+    String apiURL = "https://api.troubadour.tk/";
 
     public APIHandler(){
         api = new SpotifyApi();
     }
+
+
+    public JSONArray queryPreference(String query){
+        String url = "search?q=" + query;
+        GETCallHelper apiHelper = new GETCallHelper(url);
+        try{
+            return apiHelper.execute().get();
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+            return null;
+        }
+        catch(ExecutionException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private class GETCallHelper extends AsyncTask<Void, Void, JSONArray> {
+        //View lView;
+        private String apiQuery;
+        JSONArray jArray = null;
+
+        private GETCallHelper(String url){
+            apiQuery = url;
+        }
+
+        @Override
+        protected JSONArray doInBackground(Void... params){
+            String result = "";
+            try{
+                URL url = new URL(apiQuery);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("Get");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                //get_Data = URLEncoder.encode("func", "UTF-8") + "=" + URLEncoder.encode("denyRestaurantSubmission","UTF-8") + "&" + URLEncoder.encode("itemID", "UTF-8") + "=" + URLEncoder.encode(itemID,"UTF-8");
+                //bufferedWriter.write(get_Data);
+
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String line;
+                while((line=bufferedReader.readLine()) != null){
+                    result+=line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                jArray = encodeJSONArray(result);
+
+            }catch(MalformedURLException e){
+                e.printStackTrace();
+            }catch(IOException e){
+                e.printStackTrace();
+                e.printStackTrace();
+            }
+            return jArray;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        @SuppressWarnings({"UnusedDeclaration"})
+        protected void onPostExecute(Void Avoid){
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values){
+            super.onProgressUpdate(values);
+        }
+
+        private JSONArray encodeJSONArray(String unencodedJSON){
+            JSONArray jsonArray = null;
+            try {
+                jsonArray = new JSONArray(unencodedJSON);
+            }
+            catch(JSONException e){
+                Log.e("JSON Parse","Result: " + unencodedJSON + "|Error parsing data: " + e.toString());
+            }
+
+            return jsonArray;
+        }
+
+    }
+
 }
--- Troubadour/app/src/main/java/com/troubadour/troubadour/Activities/CreatePreferenceActivity.java
+++ Troubadour/app/src/main/java/com/troubadour/troubadour/Activities/CreatePreferenceActivity.java
@@ -1,20 +1,51 @@
 package com.troubadour.troubadour.Activities;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
+import com.troubadour.troubadour.APIHandler;
+import com.troubadour.troubadour.Adapters.PreferenceListAdapter;
+import com.troubadour.troubadour.PreferenceListItem;
 import com.troubadour.troubadour.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 public class CreatePreferenceActivity extends AppCompatActivity {
 
     /* Stub for creating a new preference
     Need to populate after API and SDK added from Spotify
      */
+
+    String apiURL = "http://api.troubadour.tk";//search?q=";
+    APIHandler api = new APIHandler();
+    ListView prefList;
+    PreferenceListAdapter preferenceListAdapter;
+    ArrayList<PreferenceListItem> preferenceListItemArrayList = new ArrayList();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,24 +72,161 @@ public class CreatePreferenceActivity extends AppCompatActivity {
     }
 
     public void initUI(){
-        Button createButton = (Button) findViewById(R.id.createPreferenceButton);
+        final EditText queryEdit = (EditText) findViewById(R.id.prefSearchEditText);
+        Button createButton = (Button) findViewById(R.id.prefSearchButton);
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createPreference();
+                String query = queryEdit.getText().toString();
+                QueryPreferences qPreferences = new QueryPreferences(query);
+                qPreferences.execute();
             }
         });
     }
 
-    //Procedure for UI interaction (GetView 'Widget' -> Get String -> call constructor)
-    public void createPreference(){
-        EditText prefName = (EditText) findViewById(R.id.prefNameEditText);
-        EditText prefSeed = (EditText) findViewById(R.id.prefSeedEditText);
+    public void updateListView(JSONArray jsonArray){
+        prefList = (ListView) findViewById(R.id.preferenceListView);
 
-        String pName = prefName.getText().toString();
-        String pSeed = prefSeed.getText().toString();
+        try {
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject pref = jsonArray.getJSONObject(i);
+                String id = pref.getString("spotifyID");
+                String type = pref.getString("type");
+                String name = pref.getString("name");
+                String uri = pref.getString("uri");
 
-        //API Call to new pref
+                PreferenceListItem prefItem = new PreferenceListItem(name, uri);
+                preferenceListItemArrayList.add(prefItem);
+            }
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+
+        preferenceListAdapter = new PreferenceListAdapter(this,R.layout.activity_preference, preferenceListItemArrayList);
+        prefList.setAdapter(preferenceListAdapter);
+    }
+
+
+    private class QueryPreferences extends AsyncTask<Void, Void, Void> {
+        //View lView;
+        private String apiQuery;
+        JSONArray jArray = null;
+
+        private QueryPreferences(String url){
+            apiQuery = url;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params){
+            String result = "";
+            String get_Data = "";
+            try{
+                URL url = new URL(apiURL + "/search?q=" + apiQuery);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                Log.e("tag",Integer.toString(httpURLConnection.getResponseCode()));
+                //httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                //get_Data = URLEncoder.encode("/search?q=","UTF-8") + URLEncoder.encode(apiQuery,"UTF-8");
+                //bufferedWriter.write(get_Data);
+
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String line;
+                while((line=bufferedReader.readLine()) != null){
+                    result+=line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                jArray = encodeJSONArray(result);
+
+            }catch(MalformedURLException e) {
+                e.printStackTrace();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        /*
+        @Override
+        protected Void doInBackground(Void... params){
+            RESTClient
+
+            String result = "";
+            String get_Data = "";
+            try{
+                URL url = new URL(apiURL + "/search?q=" + apiQuery);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                Log.e("tag",Integer.toString(httpURLConnection.getResponseCode()));
+                //httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                //get_Data = URLEncoder.encode("/search?q=","UTF-8") + URLEncoder.encode(apiQuery,"UTF-8");
+                //bufferedWriter.write(get_Data);
+
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String line;
+                while((line=bufferedReader.readLine()) != null){
+                    result+=line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                jArray = encodeJSONArray(result);
+
+            }catch(MalformedURLException e) {
+                e.printStackTrace();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }*/
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        //@SuppressWarnings({"UnusedDeclaration"})
+        @Override
+        protected void onPostExecute(Void Avoid){
+            updateListView(jArray);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values){
+            super.onProgressUpdate(values);
+        }
+
+        private JSONArray encodeJSONArray(String unencodedJSON){
+            JSONArray jsonArray = null;
+            try {
+                jsonArray = new JSONArray(unencodedJSON);
+            }
+            catch(JSONException e){
+                Log.e("JSON Parse","Result: " + unencodedJSON + "|Error parsing data: " + e.toString());
+            }
+
+            return jsonArray;
+        }
+
     }
 
 }
