6efd9fb4e05453aa874486b8293f1be5f9cd4dd0
jblairkiel <jblairkiel@gmail.com>
2017-03-09 00:09:53 -0600

    Setup for Unit Testing

--- Troubadour/app/src/main/java/com/troubadour/troubadour/Activities/CreatePreferenceActivity.java
+++ Troubadour/app/src/main/java/com/troubadour/troubadour/Activities/CreatePreferenceActivity.java
@@ -12,16 +12,13 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-
 import com.troubadour.troubadour.CustomClasses.APIHandler;
 import com.troubadour.troubadour.Adapters.PreferenceListAdapter;
 import com.troubadour.troubadour.R;
 import com.troubadour.troubadour.CustomClasses.SpotifyObject;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
---
+++ Troubadour/app/src/test/java/com/troubadour/troubadour/TroubadourUnitTest.java
@@ -0,0 +1,18 @@
+package com.troubadour.troubadour;
+
+import com.troubadour.troubadour.CustomClasses.SpotifyObject;
+
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by James on 3/9/2017.
+ */
+
+public class TroubadourUnitTest {
+    ArrayList<SpotifyObject> testPreferences = new ArrayList<>();
+
+}
