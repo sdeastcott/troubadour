7698cf911946ae2cad7630fc14f8444107061e04
Justin Lichtle <jwlichtle@gmail.com>
2017-04-04 13:06:35 -0500

    Cleaned up login code and added switch user functionality (#42)

--- Troubadour/app/src/main/java/com/troubadour/troubadour/Activities/MainActivity.java
+++ Troubadour/app/src/main/java/com/troubadour/troubadour/Activities/MainActivity.java
@@ -1,14 +1,21 @@
 package com.troubadour.troubadour.Activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.preference.PreferenceActivity;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.troubadour.troubadour.R;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,16 +27,21 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String CLIENT_ID;
+    private String REDIRECT_URI = "troubadour://callback";
+    private int REQUEST_CODE = 1337;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initUI();
-        testLogin();
+        preferenceListener();
+        CLIENT_ID = getCLIENT_ID();
+        loginListener();
+        switchUserListener();
     }
 
-
-    public void initUI(){
+    public void preferenceListener(){
         Button preferenceListButton = (Button) findViewById(R.id.enterPreferencesButton);
         preferenceListButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,24 +51,101 @@ public class MainActivity extends AppCompatActivity {
                 preferenceIntent.setClassName("com.troubadour.troubadour","com.troubadour.troubadour.Activities.PreferenceActivity");
                 MainActivity.this.startActivity(preferenceIntent);
             }
-         });
+        });
 
     }
 
-    public void testLogin() {
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+    private void switchUserListener() {
+        Button logoutButton = (Button) findViewById(R.id.logoutButton);
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                "AuthenticationResponse", Context.MODE_PRIVATE);
+        String Token = sharedPref.getString("Token", null);
+        if(Token == null) {
+            logoutButton.setVisibility(View.GONE);
+        }
+        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-                loginIntent.setClassName("com.troubadour.troubadour","com.troubadour.troubadour.Activities.LoginActivity");
-                MainActivity.this.startActivity(loginIntent);
+                switchUser();
             }
         });
     }
 
-    @SuppressWarnings("unused")
-    public void displaySecret(){
+    private void switchUser() {
+        String REDIRECT_URI = "troubadour://callback";
+        int REQUEST_CODE = 1337;
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(getCLIENT_ID(),
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setShowDialog(true);
+        builder.setScopes(new String[]{"user-read-private", "streaming", "playlist-modify-public"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    public void loginListener() {
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                "AuthenticationResponse", Context.MODE_PRIVATE);
+        String Token = sharedPref.getString("Token", null);
+        if(Token != null) {
+            loginButton.setVisibility(View.GONE);
+        }
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming", "playlist-modify-public"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    //to catch the callback from the Spotify login activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                case TOKEN:
+                    Context context = getApplicationContext();
+                    SharedPreferences sharedPref = context.getSharedPreferences(
+                            "AuthenticationResponse", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString("Token", response.getAccessToken());
+                    editor.commit();
+                    Button loginButton = (Button) findViewById(R.id.loginButton);
+                    Button logoutButton = (Button) findViewById(R.id.logoutButton);
+                    loginButton.setVisibility(View.GONE);
+                    logoutButton.setVisibility(View.VISIBLE);
+
+                    break;
+
+                case ERROR:
+                    System.out.println(response.toString());
+                    break;
+
+
+                default:
+                    //do nothing
+            }
+        }
+    }
+
+
+
+    public String getCLIENT_ID(){
 
         // Reading json file from assets folder
         BufferedReader br = null;
@@ -89,16 +178,12 @@ public class MainActivity extends AppCompatActivity {
             JSONObject jsonResponse = new JSONObject(input);
             JSONArray jsonMainNode = jsonResponse.optJSONArray("spotifyAppCred");
 
-            //Display first Secret
-                JSONObject jsonChildNode = jsonMainNode.getJSONObject(0);
-                String cID = jsonChildNode.optString("ClientID");
-                String cSecret = jsonChildNode.optString("ClientSecret");
-            //Toast.makeText(this, "ClientID: '" +cID+ "' | ClientSecret: '" + cSecret, Toast.LENGTH_LONG).show();
-            //String android_id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-            //Toast.makeText(this, "Android ID: " + android_id, Toast.LENGTH_LONG);
+            JSONObject jsonChildNode = jsonMainNode.getJSONObject(0);
+            return jsonChildNode.optString("ClientID");
         }
         catch(JSONException e){
             Toast.makeText(this, "Error"+e.toString(), Toast.LENGTH_SHORT).show();
+            return null;
         }
     }
 }
