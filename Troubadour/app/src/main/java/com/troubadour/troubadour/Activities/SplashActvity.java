80ad8a2fb98586e08e2c3295c21e5552a1daf662
Blair Kiel <jblairkiel@gmail.com>
2017-04-17 20:35:59 -0500

    Splash screen (#60)
    * Added Splashscreen / Need to insert logo later
    
    * Added Splashscreen / Need to insert logo later

---
+++ Troubadour/app/src/main/java/com/troubadour/troubadour/Activities/SplashActvity.java
@@ -0,0 +1,44 @@
+package com.troubadour.troubadour.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.troubadour.troubadour.R;
+
+public class SplashActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActvity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+        };
+        timerThread.start();
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+
+}
